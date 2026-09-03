a80a970b42e4b875949e95fd155c5ff60fc91301
Mariano Damian <51171480+mdchi@users.noreply.github.com>
2026-09-02 22:22:17 -0300

    Update 1actualizar.bat

--- 1actualizar.bat
+++ 1actualizar.bat
@@ -1,2 +1,2 @@
 curl -O https://raw.githubusercontent.com/mdchi/binance/refs/heads/main/binance.py
-curl -O https://raw.githubusercontent.com/mdchi/binance/refs/heads/main/.envpublico.py
+curl -O https://raw.githubusercontent.com/mdchi/binance/refs/heads/main/.envpublico
